4bc267679baa5da7733fcf1f7449c8620ed722ef
ruan rodrigues <rupriwell123@outlook.com>
2024-12-16 20:53:47 -0300

    Estrutura pacotes

---
+++ src/main/java/br/com/tolist/projetodevjunior/ProjetodevjuniorApplication.java
@@ -0,0 +1,13 @@
+package br.com.tolist.projetodevjunior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetodevjuniorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetodevjuniorApplication.class, args);
+	}
+
+}
