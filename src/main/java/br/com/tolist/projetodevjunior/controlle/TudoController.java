e43121906a5015f04cf05fa3cd2a1e0252e3209b
ruan rodrigues <rupriwell123@outlook.com>
2024-12-17 14:30:43 -0300

    Conexao banco de dados Msql

--- src/main/java/br/com/tolist/projetodevjunior/controlle/TudoController.java
+++ src/main/java/br/com/tolist/projetodevjunior/controlle/TudoController.java
@@ -2,7 +2,14 @@ package br.com.tolist.projetodevjunior.controlle;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.tolist.projetodevjunior.entity.Todo;
@@ -17,20 +24,20 @@ public class TudoController {
     public TudoController(TodoService todoService) {
         this.todoService = todoService;
     }
-
-    List<Todo>create(Todo todo){
+    @PostMapping
+    List<Todo>create(@RequestBody Todo todo){
         return todoService.create(todo); 
     }
-
+    @GetMapping
     List<Todo>list(){
         return todoService.list();   
     }
-
-    List<Todo>update(Todo todo){
+    @PutMapping
+    List<Todo>update(@RequestBody Todo todo){
         return todoService.update(todo);   
     }
-
-    List<Todo>delete(Long id){
+    @DeleteMapping("id")
+    List<Todo>delete(@PathVariable("id") Long id){
         return todoService.delete(id); 
     }
 
